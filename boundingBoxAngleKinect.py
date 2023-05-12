45fc054b9dd27af547028c30d8d7da9e6c163c5a
Ian Emmanuel Gonzalez Alfonzo <iagonzal97@gmail.com>
2023-05-12 13:16:27 -0500

    added a file that uses mm instead of inches

--- boundingBoxAngleKinect.py
+++ boundingBoxAngleKinect.py
@@ -84,16 +84,6 @@ def findEndPointsLineAndCorners(wood_box, color_box):
         corners = [color_box[0], color_box[3], wood_box[1], wood_box[2]]
     return endpoints, corners
 
-
-def data_for_cylinder_along_z(center_x, center_y, radius, height_z):
-    z = np.linspace(0, height_z, 50)
-    theta = np.linspace(0, 2*np.pi, 50)
-    theta_grid, z_grid = np.meshgrid(theta, z)
-    x_grid = radius*np.cos(theta_grid) + center_x
-    y_grid = radius*np.sin(theta_grid) + center_y
-    return x_grid, y_grid, z_grid
-
-
 def is_negative():
     rand_int = random.randint(0, 1)
     rand_sign = -1 if rand_int == 0 else 1
